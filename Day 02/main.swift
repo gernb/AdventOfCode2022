249f0e56e3ea6a4b7de3d310a150504d2a52c8cd
peter bohac <gern@1dot0.net>
2022-12-01 21:19:14 -0800

    Part 1

--- Day 02/main.swift
+++ Day 02/main.swift
@@ -9,11 +9,63 @@
 
 print("Day 02:")
 
+enum Move {
+    case rock, paper, scissors
+
+    var shapeScore: Int {
+        switch self {
+        case .rock: return 1
+        case .paper: return 2
+        case .scissors: return 3
+        }
+    }
+
+    static func move(for letter: String) -> Move {
+        switch letter {
+        case "A", "X": return .rock
+        case "B", "Y": return .paper
+        case "C", "Z": return .scissors
+        default:
+            fatalError()
+        }
+    }
+}
+
+struct Round {
+    let player: Move
+    let opponent: Move
+
+    var outcomeScore: Int {
+        switch (player, opponent) {
+        case (.rock, .paper): return 0 // lost
+        case (.paper, .rock): return 6 // won
+        case (.rock, .scissors): return 6 // won
+        case (.scissors, .rock): return 0 // lost
+        case (.paper, .scissors): return 0 // lost
+        case (.scissors, .paper): return 6 // won
+        default: // both are same
+            return 3 // draw
+        }
+    }
+
+    var score: Int {
+        player.shapeScore + outcomeScore
+    }
+
+    init(line: String) {
+        let parts = line.components(separatedBy: " ")
+        self.opponent = Move.move(for: parts[0])
+        self.player = Move.move(for: parts[1])
+    }
+}
+
 enum Part1 {
     static func run(_ source: InputData) {
         let input = source.data
+        let rounds = input.map(Round.init(line:))
+        let total = rounds.map(\.score).reduce(0, +)
 
-        print("Part 1 (\(source)):")
+        print("Part 1 (\(source)): \(total)")
     }
 }
 
