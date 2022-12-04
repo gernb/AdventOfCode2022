fcf0d2748353da8f975f3e46410656750083d441
peter bohac <gern@1dot0.net>
2022-12-03 21:13:49 -0800

    Part 1

--- Day 04/main.swift
+++ Day 04/main.swift
@@ -5,6 +5,31 @@
 //  Copyright © 2022 peter bohac. All rights reserved.
 //
 
+typealias Assignment = ClosedRange<Int>
+
+extension Assignment {
+    init(string: String) {
+        let ends = string.components(separatedBy: "-").compactMap(Int.init)
+        self = ends[0] ... ends [1]
+    }
+}
+
+struct Cleanup {
+    let elf1: Assignment
+    let elf2: Assignment
+
+    var fullyOverlaps: Bool {
+        (elf1.contains(elf2.lowerBound) && elf1.contains(elf2.upperBound)) ||
+         (elf2.contains(elf1.lowerBound) && elf2.contains(elf1.upperBound))
+    }
+
+    init(line: String) {
+        let parts = line.components(separatedBy: ",")
+        self.elf1 = .init(string: parts[0])
+        self.elf2 = .init(string: parts[1])
+    }
+}
+
 // MARK: - Part 1
 
 print("Day 04:")
@@ -12,8 +37,10 @@ print("Day 04:")
 enum Part1 {
     static func run(_ source: InputData) {
         let input = source.data
+        let pairs = input.map(Cleanup.init(line:))
+        let count = pairs.filter(\.fullyOverlaps).count
 
-        print("Part 1 (\(source)):")
+        print("Part 1 (\(source)): \(count)")
     }
 }
 
