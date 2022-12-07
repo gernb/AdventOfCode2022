8e26fb55e33166ad3323f4e1dde3a7c07c075f4a
peter bohac <gern@1dot0.net>
2022-12-06 22:07:46 -0800

    Part 1

--- Day 07/main.swift
+++ Day 07/main.swift
@@ -5,15 +5,125 @@
 //  Copyright © 2022 peter bohac. All rights reserved.
 //
 
+enum Command {
+    case cd(String)
+    case ls([String])
+
+    static func parse(_ lines: [String]) -> [Command] {
+        var commands: [Command] = []
+        var output: [String] = []
+        for line in lines {
+            if line.prefix(1) == "$" {
+                if output.isEmpty == false {
+                    commands.append(.ls(output))
+                    output = []
+                }
+                let parts = line.components(separatedBy: " ")
+                switch parts[1] {
+                case "cd": commands.append(.cd(parts[2]))
+                case "ls": break
+                default: fatalError()
+                }
+            } else {
+                output.append(line)
+            }
+        }
+        if output.isEmpty == false {
+            commands.append(.ls(output))
+        }
+        return commands
+    }
+}
+
+final class Directory {
+    let name: String
+    var files: [(name: String, size: Int)] = []
+    var directories: [Directory] = []
+
+    var size: Int {
+        files.map(\.size).reduce(0, +) + directories.map(\.size).reduce(0, +)
+    }
+
+    init(name: String) {
+        self.name = name
+    }
+
+    @discardableResult
+    func add(_ directory: String) -> Directory {
+        guard let dir = directories.first(where: { $0.name == directory }) else {
+            let dir = Directory(name: directory)
+            directories.append(dir)
+            return dir
+        }
+        return dir
+    }
+
+    func add(_ listing: [String]) {
+        var files: [(name: String, size: Int)] = []
+        for line in listing {
+            let parts = line.components(separatedBy: " ")
+            let name = parts[1]
+            if let size = Int(parts[0]) {
+                files.append((name, size))
+            } else {
+                assert(parts[0] == "dir")
+                add(name)
+            }
+        }
+        self.files = files
+    }
+}
+
+extension Directory: CustomStringConvertible {
+    var description: String {
+        "\(name): \(files), \(directories)"
+    }
+}
+
+extension Array where Element == Command {
+    func execute(with root: Directory) {
+        var path: [Directory] = []
+        for command in self {
+            switch command {
+            case .cd("/"):
+                path = [root]
+            case .cd(".."):
+                path.removeLast()
+            case .cd(let name):
+                let dir = path.last!.add(name)
+                path.append(dir)
+
+            case .ls(let listing):
+                path.last!.add(listing)
+            }
+        }
+    }
+}
+
 // MARK: - Part 1
 
 print("Day 07:")
 
 enum Part1 {
+    static func directories(under size: Int, startingFrom directory: Directory) -> [Directory] {
+        let subdirs = directory.directories.flatMap { directories(under: size, startingFrom: $0) }
+        if directory.size <= size {
+            return [directory] + subdirs
+        } else {
+            return subdirs
+        }
+    }
+
     static func run(_ source: InputData) {
         let input = source.data
+        let commands = Command.parse(input)
+        let root = Directory(name: "/")
+        commands.execute(with: root)
 
-        print("Part 1 (\(source)):")
+        let matchingDirs = directories(under: 100_000, startingFrom: root)
+        let total = matchingDirs.map(\.size).reduce(0, +)
+
+        print("Part 1 (\(source)): \(total)")
     }
 }
 
