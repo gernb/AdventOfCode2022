a11921fc1459bcfa323ddc140a6abbcfc27727d5
peter bohac <gern@1dot0.net>
2022-12-07 22:16:36 -0800

    Part 2

--- Day 08/main.swift
+++ Day 08/main.swift
@@ -22,12 +22,6 @@ struct Grid {
     }
 }
 
-extension Array where Element == Array<Int> {
-    var foo: String {
-        self.map { $0.map(String.init).joined(separator: " ") }.joined(separator: "\n")
-    }
-}
-
 // MARK: - Part 1
 
 print("Day 08:")
@@ -35,11 +29,6 @@ print("Day 08:")
 enum Part1 {
     static func run(_ source: InputData) {
         let grid = Grid(trees: source.data)
-        let v = grid.trees.indices.map { row in
-            grid.trees[row].indices.map { column in
-                grid.isVisible(row: row, column: column) ? 1 : 0
-            }
-        }
         let visible = grid.trees.indices.flatMap { row in
             grid.trees[row].indices.map { column in
                 grid.isVisible(row: row, column: column) ? 1 : 0
@@ -55,13 +44,34 @@ InputData.allCases.forEach(Part1.run)
 
 // MARK: - Part 2
 
+extension Grid {
+    func scenicScore(row: Int, column: Int) -> Int {
+        if row == 0 || row == (trees.count - 1) || column == 0 || column == (trees[row].count - 1) {
+            return 0
+        }
+        let height = trees[row][column]
+        let left = Array(trees[row][0 ..< column].reversed())
+        let right = Array(trees[row][(column + 1) ..< trees[row].count])
+        let up = Array((0 ..< row).map { trees[$0][column] }.reversed())
+        let down = Array(((row + 1) ..< trees.count).map { trees[$0][column] })
+        return [left, up, right, down]
+            .map { min($0.count, $0.prefix(while: { $0 < height }).count + 1) }
+            .reduce(1, *)
+    }
+}
+
 print("")
 
 enum Part2 {
     static func run(_ source: InputData) {
-        let input = source.data
+        let grid = Grid(trees: source.data)
+        let scores = grid.trees.indices.flatMap { row in
+            grid.trees[row].indices.map { column in
+                grid.scenicScore(row: row, column: column)
+            }
+        }
 
-        print("Part 2 (\(source)):")
+        print("Part 2 (\(source)): \(scores.max()!)")
     }
 }
 
