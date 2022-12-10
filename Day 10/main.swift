6e19cb05de6af809a177caad7f5c62a9b22c3855
peter bohac <gern@1dot0.net>
2022-12-09 21:48:50 -0800

    Part 2

--- Day 10/main.swift
+++ Day 10/main.swift
@@ -65,15 +65,60 @@ enum Part1 {
 
 InputData.allCases.forEach(Part1.run)
 
+struct Sprite {
+    let center: Int
+
+    var pixels: [Int] {
+        [center - 1, center, center + 1]
+    }
+}
+
+struct CRT {
+    var pixels: [Bool] = .init(repeating: false, count: 40 * 6)
+
+    mutating func setPixel(cycle: Int, sprint: Sprite) {
+        let rowPosition = (cycle - 1) % 40
+        pixels[cycle - 1] = sprint.pixels.contains(rowPosition)
+    }
+
+    func draw() {
+        for row in 0 ..< 6 {
+            for column in 0 ..< 40 {
+                let index = row * 40 + column
+                print(pixels[index] ? "#" : " ", terminator: "")
+            }
+            print("")
+        }
+        print("")
+    }
+}
+
 // MARK: - Part 2
 
 print("")
 
 enum Part2 {
     static func run(_ source: InputData) {
-        let input = source.data
+        let program = source.data.map(Instruction.init(line:))
+        var register = 1
+        var cycles = 0
+        var crt = CRT()
+
+        program.forEach { instruction in
+            cycles += instruction.cycles
+            switch instruction {
+            case .noop:
+                crt.setPixel(cycle: cycles, sprint: .init(center: register))
+            case .addx(let value):
+                let sprite = Sprite(center: register)
+                crt.setPixel(cycle: cycles - 1, sprint: sprite)
+                crt.setPixel(cycle: cycles, sprint: sprite)
+                register += value
+            }
+        }
 
         print("Part 2 (\(source)):")
+        crt.draw()
     }
 }
 
