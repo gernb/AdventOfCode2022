4caeaeeae1b61963c885db402c295419f604eb2a
peter bohac <gern@1dot0.net>
2022-12-09 21:27:02 -0800

    Part 1

--- Day 10/InputData.swift
+++ Day 10/InputData.swift
@@ -8,16 +8,172 @@
 import Foundation
 
 enum InputData: String, CaseIterable {
-    case example, challenge
+    case example1, example2, challenge
 
     var data: [String] {
+        string.components(separatedBy: .newlines)
+    }
+
+    var string: String {
         switch self {
 
-        case .example: return """
-""".components(separatedBy: .newlines)
+        case .example1: return """
+noop
+addx 3
+addx -5
+"""
 
-        case .challenge: return """
-""".components(separatedBy: .newlines)
+        case .example2: return """
+addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
+"""
+
+        case .challenge:
+            return try! String(contentsOfFile: ("~/Desktop/input.txt" as NSString).expandingTildeInPath)
 
         }
     }
--- Day 10/main.swift
+++ Day 10/main.swift
@@ -5,15 +5,61 @@
 //  Copyright © 2022 peter bohac. All rights reserved.
 //
 
+enum Instruction {
+    case noop
+    case addx(Int)
+
+    var cycles: Int {
+        switch self {
+        case .noop: return 1
+        case .addx: return 2
+        }
+    }
+
+    init(line: String) {
+        let parts = line.components(separatedBy: " ")
+        switch parts[0] {
+        case "noop": self = .noop
+        case "addx": self = .addx(.init(parts[1])!)
+        default: fatalError()
+        }
+    }
+}
+
 // MARK: - Part 1
 
 print("Day 10:")
 
 enum Part1 {
     static func run(_ source: InputData) {
-        let input = source.data
+        let program = source.data.map(Instruction.init(line:))
+        var register = 1
+        var cycles = 0
+        var signalStrength: [Int: Int] = [:]
 
-        print("Part 1 (\(source)):")
+        program.forEach { instruction in
+            cycles += instruction.cycles
+            switch instruction {
+            case .noop:
+                signalStrength[cycles] = cycles * register
+            case .addx(let value):
+                signalStrength[cycles - 1] = (cycles - 1) * register
+                signalStrength[cycles] = cycles * register
+                register += value
+            }
+        }
+
+        let samples = [
+            signalStrength[20, default: 0],
+            signalStrength[60, default: 0],
+            signalStrength[100, default: 0],
+            signalStrength[140, default: 0],
+            signalStrength[180, default: 0],
+            signalStrength[220, default: 0],
+        ]
+        let sum = samples.reduce(0, +)
+
+        print("Part 1 (\(source)): \(sum)")
     }
 }
 
