78fa83f51024f7054611c03c5cd2de776d89a849
peter bohac <gern@1dot0.net>
2022-12-12 22:44:14 -0800

    Part 1

--- Day 13/InputData.swift
+++ Day 13/InputData.swift
@@ -10,14 +10,43 @@ import Foundation
 enum InputData: String, CaseIterable {
     case example, challenge
 
+    var pairs: [ArraySlice<String>] {
+        self.data.split(separator: "")
+    }
+
     var data: [String] {
         switch self {
 
         case .example: return """
+[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]
 """.components(separatedBy: .newlines)
 
-        case .challenge: return """
-""".components(separatedBy: .newlines)
+        case .challenge:
+            return try! String(contentsOfFile: ("~/Desktop/input.txt" as NSString).expandingTildeInPath)
+                .trimmingCharacters(in: .whitespacesAndNewlines)
+                .components(separatedBy: .newlines)
 
         }
     }
--- Day 13/main.swift
+++ Day 13/main.swift
@@ -5,23 +5,105 @@
 //  Copyright © 2022 peter bohac. All rights reserved.
 //
 
-// MARK: - Part 1
+import Foundation
 
-print("Day 13:")
+enum Packet {
+    case number(Int)
+    indirect case list([Packet])
 
-enum Part1 {
-    static func run(_ source: InputData) {
-        let input = source.data
-
-        print("Part 1 (\(source)):")
+    init(_ input: inout ArraySlice<Character>) {
+        var packets = [Packet]()
+        var chars = [Character]()
+        while let char = input.popFirst() {
+            switch char {
+            case "[":
+                packets.append(.init(&input))
+                break
+            case "]":
+                if chars.isEmpty == false {
+                    let value = Int(String(chars))!
+                    packets.append(.number(value))
+                }
+                self = .list(packets)
+                return
+            case ",":
+                if chars.isEmpty == false {
+                    let value = Int(String(chars))!
+                    chars = []
+                    packets.append(.number(value))
+                }
+                break
+            default:
+                chars.append(char)
+                break
+            }
+        }
+        self = .list(packets)
     }
 }
 
+struct Pair {
+    let lhs: Packet
+    let rhs: Packet
+
+    init(lines: ArraySlice<String>) {
+        var line = Array(lines.first!)[...]
+        self.lhs = .init(&line)
+        line = Array(lines.last!)[...]
+        self.rhs = .init(&line)
+    }
+}
+
+// MARK: - Part 1
+
+extension Packet {
+    static func compare(lhs: Self, rhs: Self) -> ComparisonResult {
+        switch (lhs, rhs) {
+        case let (.number(left), .number(right)):
+            return left < right ? .orderedAscending : left > right ? .orderedDescending : .orderedSame
+        case let (.number, .list(right)):
+            return compare(lhs: [lhs], rhs: right)
+        case let (.list(left), .number):
+            return compare(lhs: left, rhs: [rhs])
+        case let (.list(left), .list(right)):
+            return compare(lhs: left, rhs: right)
+        }
+    }
+
+    private static func compare(lhs: [Packet], rhs: [Packet]) -> ComparisonResult {
+        for (index, packet) in lhs.enumerated() {
+            guard index < rhs.count else {
+                return .orderedDescending
+            }
+            let result = compare(lhs: packet, rhs: rhs[index])
+            if result == .orderedSame {
+                continue
+            } else {
+                return result
+            }
+        }
+        return lhs.count == rhs.count ? .orderedSame : .orderedAscending
+    }
+}
+
+enum Part1 {
+    static func run(_ source: InputData) {
+        let pairs = source.pairs.map(Pair.init(lines:))
+        let count = pairs.map { Packet.compare(lhs: $0.lhs, rhs: $0.rhs) }
+            .enumerated()
+            .map { index, result in
+                result == .orderedAscending ? index + 1 : 0
+            }
+            .reduce(0, +)
+
+        print("Part 1 (\(source)): \(count)")
+    }
+}
+
+print("Day 13:")
 InputData.allCases.forEach(Part1.run)
 
 // MARK: - Part 2
-
-print("")
 
 enum Part2 {
     static func run(_ source: InputData) {
@@ -31,4 +113,5 @@ enum Part2 {
     }
 }
 
+print("")
 InputData.allCases.forEach(Part2.run)
