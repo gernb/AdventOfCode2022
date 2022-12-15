45e1a1e192544e1337e09ea47cf6c6151a2edc48
peter bohac <gern@1dot0.net>
2022-12-14 21:57:47 -0800

    Part 1

--- Day 15/InputData.swift
+++ Day 15/InputData.swift
@@ -10,14 +10,37 @@ import Foundation
 enum InputData: String, CaseIterable {
     case example, challenge
 
+    var row: Int {
+        switch self {
+        case .example: return 10
+        case .challenge: return 2_000_000
+        }
+    }
+
     var data: [String] {
         switch self {
 
         case .example: return """
+Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+Sensor at x=9, y=16: closest beacon is at x=10, y=16
+Sensor at x=13, y=2: closest beacon is at x=15, y=3
+Sensor at x=12, y=14: closest beacon is at x=10, y=16
+Sensor at x=10, y=20: closest beacon is at x=10, y=16
+Sensor at x=14, y=17: closest beacon is at x=10, y=16
+Sensor at x=8, y=7: closest beacon is at x=2, y=10
+Sensor at x=2, y=0: closest beacon is at x=2, y=10
+Sensor at x=0, y=11: closest beacon is at x=2, y=10
+Sensor at x=20, y=14: closest beacon is at x=25, y=17
+Sensor at x=17, y=20: closest beacon is at x=21, y=22
+Sensor at x=16, y=7: closest beacon is at x=15, y=3
+Sensor at x=14, y=3: closest beacon is at x=15, y=3
+Sensor at x=20, y=1: closest beacon is at x=15, y=3
 """.components(separatedBy: .newlines)
 
-        case .challenge: return """
-""".components(separatedBy: .newlines)
+        case .challenge:
+            return try! String(contentsOfFile: ("~/Desktop/input.txt" as NSString).expandingTildeInPath)
+                .trimmingCharacters(in: .whitespacesAndNewlines)
+                .components(separatedBy: .newlines)
 
         }
     }
--- Day 15/main.swift
+++ Day 15/main.swift
@@ -5,23 +5,134 @@
 //  Copyright © 2022 peter bohac. All rights reserved.
 //
 
-// MARK: - Part 1
+struct Position: Hashable, CustomStringConvertible {
+    var x: Int
+    var y: Int
 
-print("Day 15:")
+//    static let origin: Self = .init(x: 0, y: 0)
 
-enum Part1 {
-    static func run(_ source: InputData) {
-        let input = source.data
+    var description: String { "(\(x), \(y))" }
 
-        print("Part 1 (\(source)):")
+    var up: Self { .init(x: x, y: y - 1) }
+    var down: Self { .init(x: x, y: y + 1) }
+    var left: Self { .init(x: x - 1, y: y) }
+    var right: Self { .init(x: x + 1, y: y) }
+    var upLeft: Self { .init(x: x - 1, y: y - 1) }
+    var upRight: Self { .init(x: x + 1, y: y - 1) }
+    var downLeft: Self { .init(x: x - 1, y: y + 1) }
+    var downRight: Self { .init(x: x + 1, y: y + 1) }
+
+    var adjacent: [Self] { [upLeft, up, upRight, left, right, downLeft, down, downRight] }
+//    var neighbours: [Self] { [up, left, right, down] }
+
+    func distance(to other: Self) -> Int {
+        return abs(x - other.x) + abs(y - other.y)
     }
 }
 
+extension Collection where Element: Comparable {
+    func range() -> ClosedRange<Element> {
+        precondition(count > 0)
+        let sorted = self.sorted()
+        return sorted.first! ... sorted.last!
+    }
+}
+
+extension Dictionary where Key == Position {
+    var xRange: ClosedRange<Int> { keys.map { $0.x }.range() }
+    var yRange: ClosedRange<Int> { keys.map { $0.y }.range() }
+
+    func draw(default: Value, overwrite: Bool = true) {
+        let xRange = self.xRange
+        let yRange = self.yRange
+        if overwrite {
+            print("\u{001b}[H") // send the cursor home
+        }
+        for y in yRange {
+            for x in xRange {
+                let pixel = self[Key(x: x, y: y), default: `default`]
+                print(pixel, terminator: "")
+            }
+            print("")
+        }
+    }
+}
+
+enum Item: String, CustomStringConvertible {
+    case sensor = "S"
+    case beacon = "B"
+    case empty = " "
+
+    var description: String { self.rawValue }
+}
+
+struct Pair {
+    let sensor: Position
+    let beacon: Position
+
+    var distance: Int { sensor.distance(to: beacon) }
+    var xRange: ClosedRange<Int> { (sensor.x - distance - 1) ... (sensor.x + distance + 1) }
+    var yRange: ClosedRange<Int> { (sensor.y - distance - 1) ... (sensor.y + distance + 1) }
+
+    func insideRadius(_ position: Position) -> Bool {
+        return position.distance(to: sensor) <= distance
+    }
+}
+
+func parseInput(_ lines: [String]) -> (pairs: [Pair], map: [Position: Item]) {
+    let pairs = lines.map { line in
+        let parts = line.components(separatedBy: " ")
+        var value = String(parts[2].dropFirst(2).dropLast())
+        var x = Int(value)!
+        value = String(parts[3].dropFirst(2).dropLast())
+        var y = Int(value)!
+        let sensor = Position(x: x, y: y)
+        value = String(parts[8].dropFirst(2).dropLast())
+        x = Int(value)!
+        value = String(parts[9].dropFirst(2))
+        y = Int(value)!
+        let beacon = Position(x: x, y: y)
+        return Pair(sensor: sensor, beacon: beacon)
+    }
+    var map: [Position: Item] = [:]
+    pairs.forEach { pair in
+        map[pair.sensor] = .sensor
+        map[pair.beacon] = .beacon
+    }
+    return (pairs, map)
+}
+
+// MARK: - Part 1
+
+enum Part1 {
+    static func run(_ source: InputData) {
+        let (pairs, map) = parseInput(source.data)
+        let contributingPairs = pairs.filter { $0.yRange.contains(source.row) }
+        let xValues = contributingPairs.flatMap { [$0.xRange.lowerBound, $0.xRange.upperBound] }.sorted()
+        let xRange = (xValues.first! ... xValues.last!)
+
+        var count = 0
+        for x in xRange {
+            let position = Position(x: x, y: source.row)
+            guard map.keys.contains(position) == false else {
+                continue
+            }
+            for pair in contributingPairs {
+                if pair.insideRadius(position) {
+                    count += 1
+                    break
+                }
+            }
+        }
+
+        print("Part 1 (\(source)): \(count)")
+    }
+}
+
+print("Day 15:")
 InputData.allCases.forEach(Part1.run)
 
 // MARK: - Part 2
-
-print("")
 
 enum Part2 {
     static func run(_ source: InputData) {
@@ -31,4 +142,5 @@ enum Part2 {
     }
 }
 
+print("")
 InputData.allCases.forEach(Part2.run)
