b7e263a214b95d06325f4b0f499e9f4485248e9a
peter bohac <gern@1dot0.net>
2022-12-18 11:01:57 -0800

    Improve legibility

--- Day 18/Solution.swift
+++ Day 18/Solution.swift
@@ -56,31 +56,35 @@ enum Part2 {
         let yRange: ClosedRange<Int>
         let zRange: ClosedRange<Int>
 
-        var area: Int {
+        var interiorArea: Int {
             let total = cubes.reduce(0) { result, cube in
                 result + cube.allDirections.filter { cubes.contains($0) == false }.count
             }
-            let one = xRange.count * yRange.count * 2
-            let two = xRange.count * zRange.count * 2
-            let three = yRange.count * zRange.count * 2
-            return total - one - two - three
+            let zFace = xRange.count * yRange.count
+            let yFace = xRange.count * zRange.count
+            let xFace = yRange.count * zRange.count
+            return total - (zFace * 2 + yFace * 2 + xFace * 2)
         }
 
         func isInside(_ cube: Position) -> Bool {
             xRange.contains(cube.x) && yRange.contains(cube.y) && zRange.contains(cube.z)
         }
 
-        mutating func fillNooks() -> Bool {
-            var didFill = false
-            cubes.forEach { cube in
+        func isVoid(_ cube: Position) -> Bool {
+            isInside(cube) && cubes.contains(cube) == false && rock.contains(cube) == false
+        }
+
+        mutating func fillVoids() {
+            var queue = cubes
+            while queue.isEmpty == false {
+                let cube = queue.removeFirst()
                 cube.allDirections.forEach { adjacent in
-                    if isInside(adjacent) && cubes.contains(adjacent) == false && rock.contains(adjacent) == false {
-                        didFill = true
+                    if isVoid(adjacent) {
                         cubes.insert(adjacent)
+                        queue.insert(adjacent)
                     }
                 }
             }
-            return didFill
         }
     }
 
@@ -98,48 +102,38 @@ enum Part2 {
             return (values.first! - 1) ... (values.last! + 1)
         }()
 
-        var xExteriorCubes: Set<Position> = []
+        var exteriorCubes: Set<Position> = []
+        func addLine(_ line: [Position]) {
+            if let first = line.firstIndex(where: { cubes.contains($0) }),
+               let last = line.lastIndex(where: { cubes.contains($0) }) {
+                exteriorCubes.formUnion(line[0 ..< first])
+                exteriorCubes.formUnion(line[(last + 1 ..< line.count)])
+            } else {
+                exteriorCubes.formUnion(line)
+            }
+        }
+
         for z in zRange {
             for y in yRange {
                 let line = xRange.map { Position(x: $0, y: y, z: z) }
-                if let first = line.firstIndex(where: { cubes.contains($0) }),
-                   let last = line.lastIndex(where: { cubes.contains($0) }) {
-                    xExteriorCubes.formUnion(line[0 ..< first])
-                    xExteriorCubes.formUnion(line[(last + 1 ..< line.count)])
-                } else {
-                    xExteriorCubes.formUnion(line)
-                }
+                addLine(line)
             }
         }
-        var yExteriorCubes: Set<Position> = []
         for z in zRange {
             for x in xRange {
                 let line = yRange.map { Position(x: x, y: $0, z: z) }
-                if let first = line.firstIndex(where: { cubes.contains($0) }),
-                   let last = line.lastIndex(where: { cubes.contains($0) }) {
-                    yExteriorCubes.formUnion(line[0 ..< first])
-                    yExteriorCubes.formUnion(line[(last + 1 ..< line.count)])
-                } else {
-                    yExteriorCubes.formUnion(line)
-                }
+                addLine(line)
             }
         }
-        var zExteriorCubes: Set<Position> = []
         for y in yRange {
             for x in xRange {
                 let line = zRange.map { Position(x: x, y: y, z: $0) }
-                if let first = line.firstIndex(where: { cubes.contains($0) }),
-                   let last = line.lastIndex(where: { cubes.contains($0) }) {
-                    zExteriorCubes.formUnion(line[0 ..< first])
-                    zExteriorCubes.formUnion(line[(last + 1 ..< line.count)])
-                } else {
-                    zExteriorCubes.formUnion(line)
-                }
+                addLine(line)
             }
         }
 
         return .init(
-            cubes: xExteriorCubes.union(yExteriorCubes).union(zExteriorCubes),
+            cubes: exteriorCubes,
             rock: cubes,
             xRange: xRange,
             yRange: yRange,
@@ -150,10 +144,8 @@ enum Part2 {
     static func run(_ source: InputData) {
         let cubes = Set(source.data.map(Position.init(line:)))
         var water = exteriorFill(cubes)
-        while water.fillNooks() {
-            print("Added cube(s)")
-        }
+        water.fillVoids()
 
-        print("Part 2 (\(source)): \(water.area)")
+        print("Part 2 (\(source)): \(water.interiorArea)")
     }
 }
