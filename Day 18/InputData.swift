0d01e01744653f90f7601f4db0a4a14350d678b4
peter bohac <gern@1dot0.net>
2022-12-17 21:27:03 -0800

    Part 1

--- Day 18/InputData.swift
+++ Day 18/InputData.swift
@@ -16,6 +16,19 @@ enum InputData: String, CaseIterable {
         switch self {
 
         case .example: return """
+2,2,2
+1,2,2
+3,2,2
+2,1,2
+2,3,2
+2,2,1
+2,2,3
+2,2,4
+2,2,6
+1,2,5
+3,2,5
+2,1,5
+2,3,5
 """.components(separatedBy: .newlines)
 
         case .challenge:
--- Day 18/Solution.swift
+++ Day 18/Solution.swift
@@ -5,13 +5,44 @@
 //  Copyright © 2022 peter bohac. All rights reserved.
 //
 
+struct Position: Hashable, CustomStringConvertible {
+    var x: Int
+    var y: Int
+    var z: Int
+
+    var description: String { "\(x),\(y),\(z)" }
+
+    var up: Self { .init(x: x, y: y + 1, z: z) }
+    var down: Self { .init(x: x, y: y - 1, z: z) }
+    var left: Self { .init(x: x - 1, y: y, z: z) }
+    var right: Self { .init(x: x + 1, y: y, z: z) }
+    var `in`: Self { .init(x: x, y: y, z: z + 1) }
+    var `out`: Self { .init(x: x, y: y, z: z - 1) }
+
+    var allDirections: [Self] {
+        [up, down, left, right, `in`, `out`]
+    }
+}
+
+extension Position {
+    init(line: String) {
+        let parts = line.components(separatedBy: ",")
+        self.x = Int(parts[0])!
+        self.y = Int(parts[1])!
+        self.z = Int(parts[2])!
+    }
+}
+
 // MARK: - Part 1
 
 enum Part1 {
     static func run(_ source: InputData) {
-        let input = source.data
+        let cubes = Set(source.data.map(Position.init(line:)))
+        let surfaceArea = cubes.reduce(0) { result, cube in
+            result + cube.allDirections.filter { cubes.contains($0) == false }.count
+        }
 
-        print("Part 1 (\(source)):")
+        print("Part 1 (\(source)): \(surfaceArea)")
     }
 }
 
