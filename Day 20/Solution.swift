bbe22bd0adcec89e621588694239aede51e56434
peter bohac <gern@1dot0.net>
2022-12-20 09:49:24 -0800

    Part 2

--- Day 20/Solution.swift
+++ Day 20/Solution.swift
@@ -7,127 +7,12 @@
 
 // MARK: - Part 1
 
-/* UNUSED
-final class Node {
-    let id: Int
-    let value: Int
-    weak var previous: Node!
-    weak var next: Node!
-
-    init(id: Int, value: Int, previous: Node? = nil, next: Node? = nil) {
-        self.id = id
-        self.value = value
-        self.previous = previous
-        self.next = next
-    }
-}
-
-extension Node: CustomStringConvertible {
-    var description: String { "id: \(id), value: \(value)" }
-}
-
-struct List {
-    var count: Int
-    let nodes: [Node]
-    var current: Node
-
-    init(_ numbers: [Int]) {
-        self.count = numbers.count
-        self.nodes = numbers.enumerated().map { Node(id: $0.offset, value: $0.element) }
-        self.current = nodes[0]
-        for (index, node) in nodes.enumerated() {
-            if index < count - 1 {
-                node.next = nodes[index + 1]
-            } else {
-                node.next = nodes.first
-            }
-            if index == 0 {
-                node.previous = nodes.last
-            } else {
-                node.previous = nodes[index - 1]
-            }
-        }
-    }
-
-    mutating func move(toId: Int) {
-        while current.id != toId {
-            current = current.next
-        }
-    }
-
-    mutating func move(toValue: Int) {
-        while current.value != toValue {
-            current = current.next
-        }
-    }
-
-    func moveAmount(for value: Int, len: Int) -> Int {
-        abs(value) % len
-    }
-
-    mutating func move(value: Int) {
-        let direction = value < 0 ? \Node.previous : \Node.next
-        let value = moveAmount(for: value, len: count)
-        guard value > 0 else {
-            return
-        }
-        for _ in 1 ... value {
-            current = current[keyPath: direction]!
-        }
-    }
-
-    mutating func remove(id: Int) -> Node {
-        move(toId: id)
-        let next = current.next!
-        let previous = current.previous!
-        next.previous = previous
-        previous.next = next
-        count -= 1
-        return current
-    }
-
-    mutating func insert(_ node: Node) {
-        node.next = current.next
-        node.previous = current
-        current.next = node
-        node.next.previous = node
-        current = node
-        count += 1
-    }
-
-    mutating func mix() {
-        for (id, node) in nodes.enumerated() {
-            let value = node.value < 0 ? node.value - 1 : node.value
-            guard moveAmount(for: value, len: count - 1) > 0 else {
-                continue
-            }
-            let temp = remove(id: id)
-            move(value: value)
-            insert(temp)
-        }
-    }
-}
-
-extension List {
-    func dump() -> [Int] {
-        var numbers: [Int] = []
-        var node = current.next!
-        let startId = node.id
-        repeat {
-            numbers.append(node.value)
-            node = node.next
-        } while node.id != startId
-        return numbers
-    }
-}
-*/
-
 enum Part1 {
-    static func mix(_ numbers: [Int]) -> [Int] {
-        let encryptedFile = numbers.enumerated().map { (value: $0.element, id: $0.offset) }
+    typealias EncryptedFile = [(value: Int, id: Int)]
+    static func mix(_ encryptedFile: EncryptedFile, with source: EncryptedFile) -> EncryptedFile {
         let count = encryptedFile.count - 1
         var decryptedFile = encryptedFile
-        for number in encryptedFile {
+        for number in source {
             guard number.value != 0 else { continue }
             let index = decryptedFile.firstIndex(where: { $0.id == number.id })!
             decryptedFile.remove(at: index)
@@ -140,11 +25,12 @@ enum Part1 {
             }
             decryptedFile.insert(number, at: newIndex)
         }
-        return decryptedFile.map(\.value)
+        return decryptedFile
     }
 
     static func run(_ source: InputData) {
-        let decryptedFile = mix(source.numbers)
+        let original = source.numbers.enumerated().map { (value: $0.element, id: $0.offset) }
+        let decryptedFile = mix(original, with: original).map(\.value)
         let indexOfZero = decryptedFile.firstIndex(of: 0)!
         let coords = [1000, 2000, 3000].map {
             decryptedFile[(indexOfZero + $0) % decryptedFile.count]
@@ -159,8 +45,19 @@ enum Part1 {
 
 enum Part2 {
     static func run(_ source: InputData) {
-        let encryptedFile = source.numbers
+        let decryptionKey = 811589153
+        let original = source.numbers.enumerated().map { (value: $0.element * decryptionKey, id: $0.offset) }
+        var decryptedNumbers = original
+        for _ in 1 ... 10 {
+            decryptedNumbers = Part1.mix(decryptedNumbers, with: original)
+        }
+        let decryptedFile = decryptedNumbers.map(\.value)
+        let indexOfZero = decryptedFile.firstIndex(of: 0)!
+        let coords = [1000, 2000, 3000].map {
+            decryptedFile[(indexOfZero + $0) % decryptedFile.count]
+        }
+        print(coords)
 
-        print("Part 2 (\(source)):")
+        print("Part 2 (\(source)): \(coords.reduce(0, +))")
     }
 }
