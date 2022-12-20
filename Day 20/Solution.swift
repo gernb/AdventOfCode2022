24adf3a3250b31ca45e26f66d6a88cff540244a8
peter bohac <gern@1dot0.net>
2022-12-20 10:23:26 -0800

    Simplify

--- Day 20/Solution.swift
+++ Day 20/Solution.swift
@@ -9,13 +9,12 @@
 
 enum Part1 {
     typealias EncryptedFile = [(value: Int, id: Int)]
-    static func mix(_ encryptedFile: EncryptedFile, with source: EncryptedFile) -> EncryptedFile {
+    static func mix(_ encryptedFile: EncryptedFile) -> EncryptedFile {
         let count = encryptedFile.count - 1
         var decryptedFile = encryptedFile
-        for number in source {
-            guard number.value != 0 else { continue }
-            let index = decryptedFile.firstIndex(where: { $0.id == number.id })!
-            decryptedFile.remove(at: index)
+        for id in encryptedFile.indices {
+            let index = decryptedFile.firstIndex(where: { $0.id == id })!
+            let number = decryptedFile[index]
             var newIndex = (index + number.value) % count
             if newIndex < 0 {
                 newIndex = count + newIndex
@@ -23,14 +22,16 @@ enum Part1 {
             if newIndex == 0 {
                 newIndex = count
             }
+            guard newIndex != index else { continue }
+            decryptedFile.remove(at: index)
             decryptedFile.insert(number, at: newIndex)
         }
         return decryptedFile
     }
 
     static func run(_ source: InputData) {
-        let original = source.numbers.enumerated().map { (value: $0.element, id: $0.offset) }
-        let decryptedFile = mix(original, with: original).map(\.value)
+        let encryptedFile = source.numbers.enumerated().map { (value: $0.element, id: $0.offset) }
+        let decryptedFile = mix(encryptedFile).map(\.value)
         let indexOfZero = decryptedFile.firstIndex(of: 0)!
         let coords = [1000, 2000, 3000].map {
             decryptedFile[(indexOfZero + $0) % decryptedFile.count]
@@ -46,10 +47,9 @@ enum Part1 {
 enum Part2 {
     static func run(_ source: InputData) {
         let decryptionKey = 811589153
-        let original = source.numbers.enumerated().map { (value: $0.element * decryptionKey, id: $0.offset) }
-        var decryptedNumbers = original
+        var decryptedNumbers = source.numbers.enumerated().map { (value: $0.element * decryptionKey, id: $0.offset) }
         for _ in 1 ... 10 {
-            decryptedNumbers = Part1.mix(decryptedNumbers, with: original)
+            decryptedNumbers = Part1.mix(decryptedNumbers)
         }
         let decryptedFile = decryptedNumbers.map(\.value)
         let indexOfZero = decryptedFile.firstIndex(of: 0)!
